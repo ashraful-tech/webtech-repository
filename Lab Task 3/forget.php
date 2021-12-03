276b9a6b6026fee6cf5bdc2d43e60ff42331d463
ashraful-tech <islam.ashraful.cse@gmail.com>
2021-12-04 00:17:36 +0600

    4

--- Lab Task 3/forget.php
+++ Lab Task 3/forget.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
+	<title>Change Password</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>              
 </head>       
 <body>
 	<br />
-	<div class="container" style="width: 250px;">
-		<h3 align="center">Login to</h3>
+	<div class="container" style="width: 350px;">
+		<h3 align="center">Change Password</h3>
 		<br />
 
-		<form method="POST">
+		<form method="POST" action="login.php">
 			
 			<br />
-			<label>User name</label>
-			<input type="text" name="Username" class="form-control" /> <br />
+			<label>Current password</label>
+			<input type="password" name="cur_pass" class="form-control" /> <br />
 
 			<label>Password</label>
 			<input type="password" name="password" class="form-control" /> <br />
 
+			<label>Re-Type Password</label>
+			<input type="password" name="password" class="form-control" /> <br />
+
 			<!-- <select></select> -->
-			<input type="checkbox" name="checkbox">
-			Remember me
+			<!-- <input type="checkbox" name="checkbox">
+			Remember me -->
 
 			<br />
 			<input type="submit" name="submit" value="submit">
-			<a href="forget.php">forget password </a>
 
 		</form>
 	</div>
